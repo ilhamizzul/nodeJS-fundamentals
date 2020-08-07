9e4e54749c8b8a6bb25795541319d9256f0bb5c4
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-08-07 11:02:13 +0700

    express sessions part 2

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const schema = mongoose.Schema;
+
+const user = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('user', user)
