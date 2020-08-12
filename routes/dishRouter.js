f5b8300a9d3d88663d4174b57117ab897e66de2b
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2020-08-12 15:56:13 +0700

    mongoose population

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -11,7 +11,8 @@ dishRouter.use(bodyParser.json())
 
 dishRouter.route('/')
 .get((req, res, next) => {
-    Dishes.find({}).then((dishes) => {
+    Dishes.find({}).populate('comments.author')
+    .then((dishes) => {
         res.statusCode = 200
         res.setHeader("Content-Type", 'application/json')
         res.json(dishes);
@@ -42,7 +43,8 @@ dishRouter.route('/')
 
 dishRouter.route('/:dishId')
 .get((req, res, next) => {
-    Dishes.findById(req.params.dishId).then((dish) => {
+    Dishes.findById(req.params.dishId).populate('comments.author')
+    .then((dish) => {
         res.statusCode = 200
         res.setHeader("Content-Type", 'application/json')
         res.json(dish)
@@ -72,7 +74,8 @@ dishRouter.route('/:dishId')
 
 dishRouter.route('/:dishId/comments')
 .get((req, res, next) => {
-    Dishes.findById(req.params.dishId).then((dish) => {
+    Dishes.findById(req.params.dishId).populate('comments.author')
+    .then((dish) => {
         if (dish != null) {
             res.statusCode = 200
             res.setHeader("Content-Type", 'application/json')
@@ -88,11 +91,14 @@ dishRouter.route('/:dishId/comments')
 .post(authenticate.verifyUser, (req, res, next) => {
     Dishes.findById(req.params.dishId).then((dish) => {
         if (dish != null) {
+            req.body.author = req.user._id
             dish.comments.push(req.body)
             dish.save().then((dish) => {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'application/json')
-                res.json(dish)
+                Dishes.findById(dish._id).populate('comments.author').then((dish) => {
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json(dish)
+                })
             }, (err) => next(err))
         } else {
             err = new Error('Dish '+ req.params.dishId+ ' not found!')
@@ -128,7 +134,8 @@ dishRouter.route('/:dishId/comments')
 
 dishRouter.route('/:dishId/comments/:commentId')
 .get((req, res, next) => {
-    Dishes.findById(req.params.dishId).then((dish) => {
+    Dishes.findById(req.params.dishId).populate('comments.author')
+    .then((dish) => {
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             res.statusCode = 200
             res.setHeader("Content-Type", 'application/json')
@@ -159,9 +166,11 @@ dishRouter.route('/:dishId/comments/:commentId')
                 dish.comments.id(req.params.commentId).comment = req.body.comment
             }
             dish.save().then((dish) => {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'application/json')
-                res.json(dish)
+                Dishes.findById(dish._id).populate('comments.author').then((dish) => {
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json(dish)
+                })
             }, (err) => next(err))
         } else if (dish == null) {
             err = new Error('Dish '+ req.params.dishId+ ' not found!')
@@ -180,9 +189,11 @@ dishRouter.route('/:dishId/comments/:commentId')
         if (dish != null && dish.comments.id(req.params.commentId) != null) {
             dish.comments.id(req.params.commentId).remove()
             dish.save().then((dish) => {
-                res.statusCode = 200
-                res.setHeader('Content-Type', 'application/json')
-                res.json(dish)
+                Dishes.findById(dish._id).populate('comments.author').then((dish) => {
+                    res.statusCode = 200
+                    res.setHeader('Content-Type', 'application/json')
+                    res.json(dish)
+                })
             }, (err) => next(err))
         } else if (dish == null) {
             err = new Error('Dish '+ req.params.dishId+ ' not found!')
